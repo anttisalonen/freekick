6e751a5bb061a7521c87972ac43a067f9fff1cbe
Antti Salonen <ajsalonen@gmail.com>
2009-03-11 13:04:33 +0100

    libmatch: added Constants.h

---
+++ src/freekick/match/Constants.h
@@ -0,0 +1,35 @@
+/************************************************************************
+  This file is part of Freekick.
+
+  Freekick is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Freekick is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with Freekick.  If not, see <http://www.gnu.org/licenses/>.
+
+  Copyright Antti Salonen, 2008
+**************************************************************************/
+
+
+#ifndef FREEKICKMATCH_CONSTANTS_H
+#define FREEKICKMATCH_CONSTANTS_H
+
+namespace freekick
+{
+    namespace match
+    {
+        namespace constants
+        {
+            static const float max_kick_distance = 1.2f;
+        }
+    }
+}
+
+#endif
